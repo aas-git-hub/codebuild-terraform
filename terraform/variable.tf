45d3d5abfcf43b712d94be7d690b38c8197192a1
andrews <novadina@hotmail.com>
2025-10-25 04:14:14 +0700

    added terraform folder and files. Added buildspec.yml

---
+++ terraform/variable.tf
@@ -0,0 +1,33 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy into"
+  default     = "ca-central-1"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "AMI ID to use for the instance"
+  default     = "ami-03814457ed908d8f6"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "EC2 Key pair name"
+  default = "andrew-keypair"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID to launch resources into"
+  default = "vpc-0ec3eaf5e3b714e89"
+}
