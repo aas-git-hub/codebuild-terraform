d40d49e4f640ea522fbe56e5f0968d25d6f5e2d4
andrews <novadina@hotmail.com>
2025-10-25 05:16:41 +0700

    change main.tf

--- terraform-source/main.tf
+++ terraform-source/main.tf
@@ -1,20 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name = var.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
-  tags = {
-    Name = "${var.environment}-server"
-  }
+# Random suffix to avoid duplicate SG names
+resource "random_id" "sg_suffix" {
+  byte_length = 2
 }
 
 resource "aws_security_group" "allow_ssh" {
-  name        = "${var.environment}_sg"
+  name        = "${var.environment}_sg_${random_id.sg_suffix.hex}"
   description = "Allow SSH inbound traffic"
   vpc_id      = var.vpc_id
 
@@ -30,5 +37,16 @@ resource "aws_security_group" "allow_ssh" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "${var.environment}-server"
   }
 }
